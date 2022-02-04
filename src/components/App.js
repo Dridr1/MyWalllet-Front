066f4e831393609e96a819bb862a87a98216a217
Adriano Fonseca <adrianof@pop-os.localdomain>
2022-02-04 17:06:56 -0300

    added auth context value

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignIn from './SignIn/Index';
 import "../Styles/style.css";
 import SignUp from './SignUp/Index';
 import Home from './Home/Index';
 import NewEntry from './NewEntry/Index';
+import NewExit from './NewExit/Index';
+import AuthContext from '../contexts/authContext';
 
 export default function App() {
+
+    const [token, setToken] = useState();
+
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<SignIn />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/new-entry" element={<NewEntry />} />
-            </Routes>
+            <AuthContext.Provider value={{ token, setToken }}>
+                <Routes>
+                    <Route path="/" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/new-entry" element={<NewEntry />} />
+                    <Route path="/new-exit" element={<NewExit />} />
+                </Routes>
+            </AuthContext.Provider>
         </BrowserRouter>
     );
 }
