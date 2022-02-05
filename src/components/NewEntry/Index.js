ac6707f5ca345ff7263d8677d618e37bd2d44f65
Adriano Fonseca <adrianof@pop-os.localdomain>
2022-02-04 23:57:36 -0300

    feat: added integration to the API

--- src/components/NewEntry/Index.js
+++ src/components/NewEntry/Index.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../contexts/authContext';
+import { newMovement } from '../../services/api';
 import { Form, Input, Button } from '../SignIn/Styles';
 import {Container, SubTitle } from './Styles';
 
 export default function NewEntry() {
+    const navigate = useNavigate();
+
+    const {token} = useContext(AuthContext);
+
+    const [entryData, setEntryData] = useState({description: '', value: '', type: 'entry'});
+
+    const handleChange = e => { 
+        setEntryData({...entryData, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const promise = newMovement(entryData, token);
+
+        promise.then( () => {
+            navigate('/home');
+        }).catch(err => console.log(err));
+    }
+
     return (
         <Container>
             <SubTitle>
                 <h2>Nova entrada</h2>
                 </SubTitle>
-            <Form>
-                <Input placeholder='Valor' type='text' />
-                <Input placeholder='Descrição' type='text' />
+            <Form onSubmit={handleSubmit}>
+                <Input onChange={handleChange} name='value' placeholder='Valor' type='text' />
+                <Input onChange={handleChange} name='description' placeholder='Descrição' type='text' />
                 <Button>Salvar entrada</Button>
             </Form>
         </Container>
--- src/components/NewExit/Index.js
+++ src/components/NewExit/Index.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../contexts/authContext';
+import { newMovement } from '../../services/api';
 import { Container, SubTitle } from '../NewEntry/Styles';
 import { Input, Button, Form } from '../SignIn/Styles';
+
 export default function NewExit() {
+    const navigate = useNavigate();
+
+    const {token} = useContext(AuthContext);
+
+    const [entryData, setEntryData] = useState({description: '', value: '', type: 'exit'});
+
+    const handleChange = e => { 
+        setEntryData({...entryData, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const promise = newMovement(entryData, token);
+        promise.then( () => {
+            navigate('/home');
+        }).catch(err => console.log(err));
+    }
+
     return (
         <Container>
             <SubTitle>
                 <h2>Nova saída</h2>
-            </SubTitle>
-            <Form>
-                <Input placeholder='Valor' type='text' />
-                <Input placeholder='Descrição' type='text' />
-                <Button>Salvar saída</Button>
+                </SubTitle>
+            <Form onSubmit={handleSubmit}>
+                <Input onChange={handleChange} name='value' placeholder='Valor' type='text' />
+                <Input onChange={handleChange} name='description' placeholder='Descrição' type='text' />
+                <Button>Salvar entrada</Button>
             </Form>
         </Container>
     );
