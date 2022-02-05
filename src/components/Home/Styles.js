206a3a19262f0200cd72a35ae97eb304878c9b9b
Adriano Fonseca <adrianof@pop-os.localdomain>
2022-02-04 23:55:14 -0300

    feat: added styling and integration to the API and Data Base

--- src/components/Home/Styles.js
+++ src/components/Home/Styles.js
@@ -29,6 +29,16 @@ const Registers = styled.div`
     background: white;
     border-radius: 5px;
     margin-top: 26px;
+    padding: 23px 12px 10px 12px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    font-family: 'Raleway';
+    font-size: 20px;
+    line-height: 23px;
+    text-align: center;
+    position: relative;
 `;
 
 const Options = styled.div`
@@ -56,9 +66,90 @@ const Options = styled.div`
     }
 `;
 
+const MovContainer = styled.div`
+    height: 92%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    overflow: auto;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    ::-webkit-scrollbar {
+        display: none;
+    }
+`;
+
+const Mov = styled.div`
+    height: 25px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const MovInfo = styled.div`
+    display: flex;
+    gap: 7px;
+    width: auto;
+    span{
+        color: #C6C6C6;
+        font-size: 16px;
+        line-height: 19px;
+    }
+    p{
+        font-size: 16px;
+        line-height: 19px;    
+        color: #000000;
+    }
+`;
+
+const MovValue = styled.div`
+    font-size: 16px;
+    line-height: 19px;
+    text-align: right;
+    color: ${props => props.entry ? '#5CAE13' : '#C70000'};
+`;
+
+const NoMovements = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Balance = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+`;
+
+const BalanceLegend = styled.div`
+    font-size: 17px;
+    line-height: 20px;
+    color: #000000;
+    font-weight: 700;
+`;
+
+const BalanceValue = styled.div`
+    font-size: 17px;
+    line-height: 20px;
+    text-align: right;
+    color: #03AC00;
+`;
+
 export {
     HomeContainer,
     Header,
     Registers,
-    Options
+    Options,
+    MovContainer,
+    Mov,
+    MovInfo,
+    MovValue,
+    NoMovements,
+    Balance,
+    BalanceLegend,
+    BalanceValue
 }
